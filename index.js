fa0f419d41e1e8802ff3c22bd8a109e2344288de
Harsh Patel <harshpatelhp728@gmail.com>
2022-02-16 19:49:51 -0500

    adding schema resolver for add and update employee

--- index.js
+++ index.js
@@ -1,38 +1,54 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-//import ApolloServer
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
+//import typedefs and resolvers
+const TypeDefs = require("./schema");
+const Resolvers = require("./resolvers");
+
+//import ApolloServer
+const { ApolloServer } = require("apollo-server-express");
 
 //Store sensitive information to env variables
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 //mongoDB Atlas Connection String
 const mongodb_atlas_url = process.env.MONGODB_URL;
 
 //TODO - Replace you Connection String here
-mongoose.connect(mongodb_atlas_url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(success => {
-  console.log('Success Mongodb connection')
-}).catch(err => {
-  console.log('Error Mongodb connection')
-});
+mongoose
+  .connect(mongodb_atlas_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((success) => {
+    console.log("Success Mongodb connection");
+  })
+  .catch((err) => {
+    console.log("Error Mongodb connection");
+  });
 
 //Define Apollo Server
-
+const server = new ApolloServer({
+  typeDefs: TypeDefs.typeDefs,
+  resolvers: Resolvers.resolvers,
+});
 
 //Define Express Server
 const app = express();
 app.use(bodyParser.json());
-app.use('*', cors());
+app.use("*", cors());
 
 //Add Express app as middleware to Apollo Server
+server.applyMiddleware({ app });
 
+console.log(server);
 
-//Start listen 
+//Start listen
 app.listen({ port: process.env.PORT }, () =>
-  console.log(`🚀 Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`));
+  console.log(
+    `🚀 Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`
+  )
+);
